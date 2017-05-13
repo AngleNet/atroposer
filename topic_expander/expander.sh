492e0bfbd173849c30cc7327c5c8b1a8a5bb3963
anglenet <anglenet@sina.cn>
2017-05-13 21:12:07 +0800

    update

--- topic_expander/expander.sh
+++ topic_expander/expander.sh
@@ -8,3 +8,6 @@ rm -rf atropos/data/* && rm -rf atropos/result/*
 cp -rf $tweets/data/* atropos/data # Including user_links.new
 cp -rf $tweets/trending_topics atropos/data
 cd atropos/Preprocessing && python topicExpander.py && cd -
+rm -rf data && mkdir data
+cp atropos/result/* data && cp atropos/data/trending_topics data && cp ../topics/*.topk_topic data
+tar czf data.tar.gz data
