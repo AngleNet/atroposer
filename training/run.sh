a8dc67aa8415371d42ce53de06d7e3d715b96177
anglenet <anglenet@sina.cn>
2017-05-29 23:57:10 +0800

    update

---
+++ training/run.sh
@@ -0,0 +1,14 @@
+if [ ! -d atropos ]; then
+	git clone -b topic-in-tweet git://github.com/anglenet/atropos
+else
+	cd atropos && git pull && cd -
+fi
+cp ../features/trending_index/sample atropos/data/samples.trindex
+cp ../samples/atropos/result/tweets.sample atropos/data
+cp ../users/user_links.new atropos/data/users
+if [ -d atropos/data/user_tweets ]; then
+	rm -rf atropos/data/user_tweets
+fi
+mkdir -p atropos/data/user_tweets && cp -f ../user_tweets/spide_tweets/data/* atropos/data/user_tweets
+
+cd atropos/features && python sampleGenerator.py && cd -
