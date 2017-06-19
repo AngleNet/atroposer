34b696b4f5451b50ab91bf8586b194d33839ae4b
anglenet <anglenet@sina.cn>
2017-06-19 17:19:45 +0800

    update

--- supers/topk_topics/sample.sh
+++ supers/topk_topics/sample.sh
@@ -1,37 +1,27 @@
+set -x
 proj_dir=$(pwd)/../..
 _date=$(head ${proj_dir}/date)
 if [ -d atropos ]; then
 	cd atropos && git pull && cd -
 else
-	git clone topic-in-tweet-${_date} git://github.com/anglenet/atropos
+	git clone -b topic-in-tweet git://github.com/anglenet/atropos
 fi
-cp data/${_date}.topk_topic atropos/data
+cp $proj_dir/topics/${_date}.topk_topic atropos/data
 cp data/topics.kws atropos/data
 cd atropos/Preprocessing && python topkTopicSeries.py && cd -
 cp atropos/result/${_date}.topk_topic.* data
+cp atropos/result/topics.kws.* data
 
-if [ -d result ]; then
-	echo "'result' directory already exists, remove it fistly"; 
-	exit 0
-else
-	mkdir result
-fi
-proj_dir=$(pwd)/../..
-res_dir=$(pwd)/result
-trending_index=${proj_dir}/features/trending_index
-training=${proj_dir}/training
-export NUM_RUNNER=24
-for topk in data/${_date}.topk_topic.*; do
-	_k=$(basename ${topk})
+trindex=$proj_dir/features/trending_index
+cd $trindex && bash clean.sh && cd -
+mkdir -p trindex && cd trindex
+for fn in ../data/${_date}.topk_topic.*; do 
+	_k=$(basename $fn)
 	k=${_k##*.}
-	cd ${trending_inex} && bash clean.sh && cd -
-	cp ${topk} ${trending_index}/${_date}.topk_topic
-	cp data/topics.kws.${k} ${trending_index}/topics.kws
-	cd ${trending_index} && bash trindex.sh
-	let num=NUM_RUNNER-1
-	for i in $(seq -w 00 $num); do
-		cat $i/result/sample >> sample
-	done
-	cd ${training} && bash run.sh
-	cp atropos/result/samples.train ${res_dir}/samples.train.${k}
+	mkdir -p $k
+	cp -rf $trindex/* $k
+	cp $fn $k/${_date}.topk_topic
+	cp ../data/topics.kws.$k $k/topics.kws
+	cp ../trindex_super.sh $k
+	cd $k && bash trindex_super.sh && cd -
 done
---
+++ supers/topk_topics/trindex_super.sh
@@ -0,0 +1,43 @@
+if [ ! -e topics.kws ]; then
+	echo "Extract topics' keywords firstly"
+	exit 0
+fi
+proj=$(pwd)/../../../..
+cp $proj/samples/atropos/result/tweets.sample .
+running_date=$(head $proj/date) 
+
+if [ ! -d atropos ]; then
+	git clone -b topic-in-tweet-$running_date  git://github.com/anglenet/atropos
+else
+	cd atropos && git pull && cd -
+fi
+
+lines=$(wc -l tweets.sample|awk '{print $1}')
+if [ "${NUM_RUNNER}x" != "x" ];then
+	num_runner=${NUM_RUNNER}
+else
+	num_runner=1
+fi
+let even=lines/num_runner
+let rem=lines%num_runner
+if [ ! $rem -eq 0 ]; then
+	let even=even+1
+fi
+split -l $even -d tweets.sample tweets.sample
+let num_runner=num_runner-1
+
+for i in $(seq -w 00 $num_runner); do
+	if [ -d $i ]; then
+		rm -rf $i
+	fi
+	mkdir $i
+	cp -rf atropos $i
+	cp tweets.sample$i $i/atropos/data/tweets.sample
+	cp *.topk_topic $i/atropos/data/
+	cp topics.kws $i/atropos/data
+	cp *.txt $i/atropos/resource
+	cd $i/atropos/features
+	screen /usr/bin/python3 TrendingIndex.py
+	cd -
+done
+
